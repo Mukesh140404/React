4750b68598a77be0ce2f05c7f586ec100917a052
devilking <devilking14041404@gmail.com>
2025-10-04 00:42:48 +0530

    appwrite services is done

---
+++ BlogApp/src/appwrite/auth.js
@@ -0,0 +1,53 @@
+import conf from '../conf'
+import { ID,Client,Account } from "appwrite";
+
+export class AuthServices{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email,password,name}){
+        try{
+            const user = await this.account.create({userId:ID.unique,email,password,name});
+            if(user){
+                //method call
+            }else{
+                return user
+            }
+        }catch(error){
+            throw error
+        }
+    }
+    async login({email,password}){
+        try{
+            return await createEmailPasswordSession({email,password})
+        }catch(error){
+            throw error
+        }
+    }
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+}
+
+const authServices = new AuthServices()
+
+export default authServices;
